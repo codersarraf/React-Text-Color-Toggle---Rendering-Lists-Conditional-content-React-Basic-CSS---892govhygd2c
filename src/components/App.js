fcd484922ea2203d254e677487283231338d8136
amitkr <amitsarraf6@gmail.com>
2022-12-04 18:25:56 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
-//code here 
+  const [classes, setClasses] = useState("redColor")
+  const handleStyleChange = ()=>{
+    setClasses("blueColor");
+  }
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={classes} >Newton School</p>
+      <button id='button' onClick={handleStyleChange}>Change Style</button>
     </div>
   )
 }
