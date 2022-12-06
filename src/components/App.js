0af4607f3d4eb5acbfd0ae7dcf345b7a08226b44
amitkr <amitsarraf6@gmail.com>
2022-12-07 00:51:29 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,20 @@ import '../styles/App.css';
 
 const App = () => {
   const [classes, setClasses] = useState("redColor")
+  const [value, setValue] = useState(true)
   const handleStyleChange = ()=>{
-    setClasses("blueColor");
+    setValue(!value);
   }
+  useEffect(() =>{
+    if(value){
+      setClasses("redColor")
+    }
+    if(!value){
+      setClasses("blueColor")
+    }
+
+  },[value])
+
   return (
     <div id="main">
       <p className={classes} >Newton School</p>
